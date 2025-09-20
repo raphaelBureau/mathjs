3daa1300f0fc077c1a53a5bc3aa6d69acdf952a9
raphaelBureau <94479381+raphaelBureau@users.noreply.github.com>
2025-09-20 13:21:54 -0400

    Refactor MatrixRecycler export to named export

--- matrixRecycler.js
+++ matrixRecycler.js
@@ -1,7 +1,7 @@
 //this class does matrix operations but does not allocate memory for its function calls
 //it also recycles the matrix that is transformed (the one to the right)
 //this class works for 4d matrices
-export default class MatrixRecycler {
+export class MatrixRecycler {
     constructor() {
         //using this prevents allocating 16 floats for each matrix operation
         this.intermediateValues = new Float32Array(16); //this is intended to be ran synchronously
